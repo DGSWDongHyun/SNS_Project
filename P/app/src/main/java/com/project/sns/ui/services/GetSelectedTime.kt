410da782b9df3333825534541f122340b48967fc
DGSW_양동현 <66651059+DGSWDongHyun@users.noreply.github.com>
2020-10-22 10:06:28 +0900

    [ADD] ready to get studying times

---
+++ P/app/src/main/java/com/project/sns/ui/services/GetSelectedTime.kt
@@ -0,0 +1,12 @@
+package com.project.sns.ui.services
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class GetSelectedTime : Service() {
+
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }
+}
