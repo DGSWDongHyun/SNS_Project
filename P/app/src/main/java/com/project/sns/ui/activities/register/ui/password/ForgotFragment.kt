b2013021304e4741512d96038d1487d857d9e176
DGSW_양동현 <66651059+DGSWDongHyun@users.noreply.github.com>
2020-10-23 07:55:05 +0900

    [ADD] add fragment_forgot.xml , for find password layout

---
+++ P/app/src/main/java/com/project/sns/ui/activities/register/ui/password/ForgotFragment.kt
@@ -0,0 +1,32 @@
+package com.project.sns.ui.activities.register.ui.password
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.project.sns.R
+import com.project.sns.databinding.FragmentForgotBinding
+
+class ForgotFragment : Fragment() {
+
+    var forgotBinding : FragmentForgotBinding ?= null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        forgotBinding = FragmentForgotBinding.inflate(layoutInflater)
+        return forgotBinding!!.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+            initLayout();
+    }
+    private fun initLayout(){
+
+    }
+}
